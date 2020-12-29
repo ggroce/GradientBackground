7c63ec8430688be6e533606c1f79f5112a454cac
ggroce <ggrocedev@gmail.com>
2020-12-28 21:38:51 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let pickedColor1 = color1.value;
+let pickedColor2 = color2.value;
+
+console.log(body);
+
+function setGradient() {
+	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+	css.textContent = `${body.style.background};`;
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
